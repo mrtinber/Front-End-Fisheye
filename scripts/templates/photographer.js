b52c0fa2fcab385eecad45822ad5d8682c998d39
Kevin Bertin <kevinbertinpro@gmail.com>
2024-02-28 08:14:10 +0100

    Lien avec le fichier JSON et affichage des infos supplémentaires sur la page index

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,7 +1,7 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/photographers/ID/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
@@ -9,8 +9,17 @@ function photographerTemplate(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const h3 = document.createElement('h3');
+        h3.innerText = `${city}, ${country}`; 
+        const h4 = document.createElement('h4');
+        h4.innerText = tagline;
+        const p = document.createElement('p')
+        p.innerText = `${price}€/jour`; 
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(h4);
+        article.appendChild(p);
         return (article);
     }
     return { name, picture, getUserCardDOM }
