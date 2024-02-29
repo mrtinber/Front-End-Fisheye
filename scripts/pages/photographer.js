36c03f6cf0966bd737f86f752bb0a4e404b15fd5
Kevin Bertin <kevinbertinpro@gmail.com>
2024-02-29 08:54:40 +0100

    Ajout du lien vers la page du photographe et chargement des données du photographe

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,40 @@
 //Mettre le code JavaScript lié à la page photographer.html
+
+async function getPhotographers() {
+    // Charger le fichier JSON
+    const response = await fetch('./data/photographers.json');
+
+    // Convertir la réponse en JSON
+    const photographersData = await response.json();
+    //console.log(photographersData);
+    
+    // Retourner les données des photographes
+    return photographersData;
+}
+
+async function displayData(photographers) {
+    const photographersHeader = document.querySelector(".photographer_header");
+
+    //Récupération de l'ID dans l'URL
+    let params = new URL(document.location).searchParams;
+    let photographerID = params.get("id");
+    console.log(photographerID);
+
+    // Recherche du photographe avec l'ID correspondant
+    const photographer = photographers.find(photographer => photographer.id === parseInt(photographerID));
+    console.log(photographer);
+
+    if (photographer) {
+        const photographerModel = photographerTemplate(photographer);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        photographersHeader.appendChild(userCardDOM);
+    }
+}
+
+async function init() {
+    // Récupère les datas des photographes
+    const { photographers } = await getPhotographers();
+    displayData(photographers);
+}
+
+init();
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,12 +1,19 @@
 function photographerTemplate(data) {
-    const { name, portrait, city, country, tagline, price } = data;
+    const { name, portrait, city, country, tagline, price, id } = data;
 
     const picture = `assets/photographers/ID/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+    //Création de la balise lien et attribution de l'id du photographe
+        const a = document.createElement('a');
+        a.setAttribute("href", `photographer.html?id=${id}`);
+    //Création de la balise image et de ses attributs
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+        //img.setAttribute("alt", `Portrait de ${name}`)
+        img.setAttribute("aria-label", `Photo de profil de ${name}`)
+    //Création des différentes informations liées au photographe
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
         const h3 = document.createElement('h3');
@@ -14,13 +21,15 @@ function photographerTemplate(data) {
         const h4 = document.createElement('h4');
         h4.innerText = tagline;
         const p = document.createElement('p')
-        p.innerText = `${price}€/jour`; 
-        article.appendChild(img);
+        p.innerText = `${price}€/jour`;
+    //Rattachement au code HTML
+        article.appendChild(a);
+        a.appendChild(img);
         article.appendChild(h2);
         article.appendChild(h3);
         article.appendChild(h4);
         article.appendChild(p);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, id, getUserCardDOM }
 }
